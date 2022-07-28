e879c3faf9b878f09931b2b3509e6a2f08332921
enoxie <admin@enesbilgi.com>
2022-07-28 18:15:39 +0300

    final version

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -92,7 +92,7 @@ export default Product;
 
 export const getServerSideProps = async ({ params }) => {
   const res = await axios.get(
-    `http://localhost:3000/api/products/${params.id}`
+    `https://pizza-enes.herokuapp.com/api/products/${params.id}`
   );
   return {
     props: {
